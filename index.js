b61d0d1f680e8792f8ef64755485c1135b2463de
Takuto Mitsuhashi <picos.tackt@gmail.com>
2020-07-20 13:11:31 +0900

    Create revieving phone call endpoint

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const twilio = require('twilio')
 const express = require('express')
 require('dotenv').config()
 
+const VoiceResponse = twilio.twiml.VoiceResponse
 const client = new twilio(process.env.ACCOUNT_ID, process.env.AUTH_TOKEN)
 const app = express()
 
 const TWILLIO_URL = 'http://demo.twilio.com/docs/voice.xml'
 
-// Simple phone call
-app.get('/:phone_number', function (req, res) {
+// Make a simple phone call
+app.get('/make/:phone_number', (req, res) => {
   if (req.params.phone_number) {
     const params = {
       url: TWILLIO_URL,
@@ -22,6 +23,15 @@ app.get('/:phone_number', function (req, res) {
   } else {
     res.status(500).send('phone_number is not set.')
   }
+})
+
+// Recieve a phone call
+app.get('/recieve', (req, res) => {
+  const twiml = new VoiceResponse()
+
+  twiml.say('電話してくれてありがとう')
+
+  res.status(200).header({ 'Content-Type': 'text/xml' }).send(twiml.toString())
 })
 
 const PORT = process.env.PORT || 8080
